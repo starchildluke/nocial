a29090301d86d46df2f7d11e48ac38d604d2196a
Luke Davis <luke@lukealexdavis.co.uk>
2025-09-19 00:28:29 +0100

    new port added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+	server: { port: 4323 },
 	site: 'https://emptygold.blog/',
 	trailingSlash: 'always',
 	integrations: [mdx()],
